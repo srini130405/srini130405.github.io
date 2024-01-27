e9c1692efbdb370875a6f0a360a5adb9eaf6c85f
srini130405 <141761961+srini130405@users.noreply.github.com>
2024-01-27 23:33:17 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -8,3 +8,18 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 }
+window.addEventListener('scroll',reveal);
+function reveal(){
+  var reveals=document.querySelectorAll('.section1');
+  for(var i=0;i<reveals.length;i++){
+    var windowheight= window.innerHeight;
+    var revealtop=reveals[i].getBoundingClientRect().top;
+    var revealpoint=150;
+    if(revealtop<windowheight - revealpoint){
+      reveals[i].classList.add('active');
+    }
+    else{
+      reveals[i].classList.remove('active');
+    }
+  }
+}
